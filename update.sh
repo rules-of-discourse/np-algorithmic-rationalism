d204251add68b6f8e7bbd13c148c73eefeb2579a
Sophia Shapira <Sophia.Shapira@gmail.com>
2018-05-31 01:27:35 -0400

    Created ebook distribution script

---
+++ update.sh
@@ -0,0 +1,31 @@
+
+cd "$(dirname "${0}")" || exit
+sh prepare.sh
+sourcediro="$(chobakwrap -sub realpath .)"
+
+~/com/mounty/on
+
+cd /mnt/metatemp/public_html/sites/virtualstoa-files || \
+exit
+vsfiled="$(chobakwrap -sub realpath .)"
+
+cd np-algorithmic-rationalism || exit
+outdiro="$(chobakwrap -sub realpath .)"
+cd ebkx || exit
+ebkdir="$(chobakwrap -sub realpath .)"
+
+#echo cp "${sourcediro}"/*.epub "${sourcediro}"/*.mobi "${ebkdir}/."
+cp "${sourcediro}"/algorithmic-rationalism.epub "${ebkdir}/$(cat "${sourcediro}/tmpres/datetime-cyber.txt")--algorithmic-rationalism.epub"
+cp "${sourcediro}"/algorithmic-rationalism.mobi "${ebkdir}/$(cat "${sourcediro}/tmpres/datetime-cyber.txt")--algorithmic-rationalism.mobi"
+(
+  cd "${ebkdir}" && (
+    ls *.epub *.mobi | sort
+  )
+)
+
+echo
+echo "$(cat "${sourcediro}/tmpres/smdate.txt") - $(cat "${sourcediro}/tmpres/justhour.txt")"
+
+wra "${ebkdir}/index.html"
+#wra -f "${ebkdir}/downloadpage.css"
+
