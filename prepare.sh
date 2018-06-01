6a8e94f3e308ae704bec752c76a857c216bbf456
Sophia Shapira <Sophia.Shapira@gmail.com>
2018-06-01 13:27:09 -0400

    Added many sections - though without content

--- prepare.sh
+++ prepare.sh
@@ -36,6 +36,8 @@ chobakepubtl covergen ../Images/CoverImage.jpg > Text/CoverPage.xhtml
   echo "cont:2:*:Copyright"
   echo "text:Text/mainsrc.xhtml"
   echo "cont:2:*:For the latest edition ..."
+  echo "text:Text/historical-context.xhtml"
+  echo "cont:2:*:Historical context of Algorithmic Rationalism"
   echo "text:Text/basic-principles.xhtml"
   echo "cont:2:*:Basic principles of Algorithmic Rationalism"
   echo "text:Text/expand-what-logic-means.xhtml"
@@ -43,9 +45,17 @@ chobakepubtl covergen ../Images/CoverImage.jpg > Text/CoverPage.xhtml
   echo "text:Text/use-of-algorithms.xhtml"
   echo "cont:3:*:Expansion of what the term \"logic\" means"
   echo "cvimg:Images/CoverImage.jpg"
+  echo "text:Text/training-against-biases.xhtml"
+  echo "cont:3:*:Training to overcome irrational biases"
+  echo "text:Text/a-new-etiquette.xhtml"
+  echo "cont:3:*:A new standard of etiquette"
+  echo "text:Text/among-philosophy.xhtml"
+  echo "cont:2:*:Algorithmic Rationalism in the broader world of philosophy"
   echo "text:Text/relation-to-religion.xhtml"
   echo "cont:3:*:Relation to religion"
 ) > recipe.txt
+#  echo "text:Text/xxxxxxxxx"
+#  echo "cont:3:*:xxxxxxxxxxx"
 
 
 rm -rf *.epub
