380ee822f4fe7d3624c020a875be0a3b2d0e3449
Sophia Shapira <Sophia.Shapira@gmail.com>
2018-06-04 18:46:00 -0400

    Better organization of glossary

--- glossary.sh
+++ glossary.sh
@@ -2,8 +2,12 @@
 cd "$(dirname "${0}")" || exit 2
 
 chobakepubtl bodymerge -css ../Styles/glossary.css -l \
-glsr/head.xhtml glsr/begin.xhtml $(ls -1d glsr/from-*.xhtml | sort) \
+glsr/head.xhtml $(ls -1d glsr/df-*.xhtml | sort) \
 > Text/glossary.xhtml
+
+#chobakepubtl bodymerge -css ../Styles/glossary.css -l \
+#glsr/head.xhtml glsr/begin.xhtml $(ls -1d glsr/from-*.xhtml | sort) \
+#> Text/glossary.xhtml
 
 #chobakepubtl bodymerge -css ../Styles/glossary.css -l glsr/*.xhtml \
 #> Text/glossary.xhtml
